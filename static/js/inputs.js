236658586b5dbc9f341a6ff94e76c5933b561546
michael truong <truongmt@gmail.com>
2014-02-01 12:42:16 -0800

    basic select widget

--- static/js/inputs.js
+++ static/js/inputs.js
@@ -1,27 +1,7 @@
-//configuration crap
-require.config({
-  paths: {
-    'underscore': 'vendor/underscore/underscore',
-    'backbone': 'vendor/backbone/backbone',
-    'jquery': 'vendor/jquery/jquery-2.0.3.min',
-  
-  },
-  shim: {
-    "underscore": {
-      "exports": "_"
-    },
-    "backbone": {
-      "deps": ["underscore", "jquery"],
-      "exports": "Backbone"
-    }
-  }
-});
-
 //program kickoff
 require([
   'backbone',
   'widgets/select',
-  'widgets/rate'
   ], function(Backbone, Select, Rate) {
   var prepInputs = function() {
     return $('input').val();
@@ -30,12 +10,6 @@ require([
 
 
   var populate = function(data) {
-    var n_trials_var = new Backbone.Model({rate: 0});
-    var n_rewards_var = new Backbone.Model({rate: 0});
-    var n_trials_h = new Rate({model: n_trials_var}, {title: 'N Trials', period: ''});
-    var n_reward_h = new Rate({model: n_rewards_var}, {title: 'N Reward', period: ''});
-    $('#rates').append(n_trials_h.el);
-    $('#rates').append(n_reward_h.el);
     setInterval(function() {
       var req = $.ajax({
         url: 'http://localhost:5000/data',
@@ -43,8 +17,9 @@ require([
         data: data
       });
       req.done(function(resp) {
-        n_trials_var.set('rate', resp.n_trials)
-        n_rewards_var.set('rate', resp.n_reward)
+        //set data for rates
+        //n_trials_var.set('rate', resp.n_trials)
+        //n_rewards_var.set('rate', resp.n_reward)
       });
     },1000);
   };
@@ -80,13 +55,17 @@ require([
   //kicks everything off once dom is ready 
   $(document).ready(function() {
     $(start_site);
-    var i = new Select.SelectCollection([]);
-    var s = new Select.SelectOption({collection: i});
-    $('body').append(s.el);
-    s.$el.change(function() {
-      console.log(s.getSelectedOption());
+    var req = $.ajax({
+        url: 'http://localhost:5000/list_serial_ports',
+        type: 'GET'
+    }).done(function(resp) {
+      var i = new Backbone.Collection(_.map(JSON.parse(resp), function(i) { return {foo: i}}));
+      var s = new Select({collection: i});
+      $('body').append(s.el);
+      s.$el.change(function() {
+        console.log(s.getSelectedOption());
+      })
     });
-    i.fetch({url:'http://localhost:5000/list_serial_ports', success: function(resp) {console.log('asdfasdf');console.log(resp)}} )
   });
 
   $('#gobutton').click(start_box)
