482aa251f9f796fa84715a2ef84ddfe0f86f0f75
Jeff Knowles <jknowles@Jeffs-MacBook-Pro.local>
2014-04-01 19:42:04 -0700

    pulse command support

--- arduino_sandbox/arduino_sandbox.ino
+++ arduino_sandbox/arduino_sandbox.ino
@@ -1,7 +1,8 @@
 //#include <SoftwareSerial.h>
 
 // config output pins (digital)
-byte output_pins[3] = {12, 13};
+byte output_pins[2] = {12, 13};
+//byte output_pins[2] = {12, 13};
 // config input pins (digital)
 byte input_pins[3] = {2, 3, 4};
 bool current_values[sizeof(input_pins)];
@@ -9,12 +10,12 @@ long last_input_change[sizeof(input_pins)];
 long bounce_time = 100;
 String local_buffer;
 
-bool pulse_active = 1;
-bool pulse_on = 0;
-byte pulse_trigger_pin = 10; //
-byte pulse_output_pin = 11; // pulse output pin
-long pulse_period = 100; // pulse period in ms (1000/f)
-long pulse_width = 50; // pulse width in ms
+byte pulse_mode = 1; // specify whether arduino should worry about pulse  0==off; 1==on if trigger is active; 2==on regardless of trigger
+bool pulse_on = 0;     // specifies whether pulse is running at any time
+byte pulse_trigger_pin = 10;  //
+byte pulse_output_pin = 11;   // pulse output pin
+long pulse_period = 100;      // pulse period in ms (1000/f)
+long pulse_width = 50;        // pulse width in ms
 
 
 // configure software serial ports
@@ -23,7 +24,7 @@ long pulse_width = 50; // pulse width in ms
 
 void setup() {
   // put your setup code here, to run once:
-  Serial.begin(115200);
+  Serial.begin(9600);
   //
   local_buffer.reserve(200);
   // configure input pins
@@ -94,12 +95,26 @@ void evalChunk(String chunk) {
         // if chunk indicades change to an output
         byte port = byte(chunk.substring(2, eq_idx).toInt());
         byte val = byte(chunk.substring(eq_idx + 1).toInt());
-        Serial.print(millis());
-        Serial.print( "Port ");
-        Serial.print(port);
-        Serial.print(" set to ");
-        Serial.println(val);
+        //        Serial.print(millis());
+        //        Serial.print( "Port ");
+        //        Serial.print(port);
+        //        Serial.print(" set to ");
+        //        Serial.println(val);
         digitalWrite(port, val);
+      }
+      else if (chunk.substring(1, 2).equalsIgnoreCase("p")) {
+        // if chunk indicades change to an output
+        byte val = byte(chunk.substring(eq_idx + 1).toInt());
+        pulse_mode = val;
+      }
+      else if (chunk.substring(1, 2).equalsIgnoreCase("w")) {
+        // if chunk indicades change to an output
+        pulse_width = long(chunk.substring(eq_idx + 1).toInt());
+      }
+      else if (chunk.substring(1, 2).equalsIgnoreCase("l")) {
+        // if chunk indicades change to an output
+        pulse_period = long(chunk.substring(eq_idx + 1).toInt());
+
       }
     }
     else if (chunk.equals("<sync>")) {
@@ -116,25 +131,52 @@ void loop() {
   checkInputs();
   checkSerial();
   checkBuffer();
-  //  if (millis() >= lastmillis + 1000) {
-  //    Serial.println(millis());
-  //  lastmillis = millis();}
-  if (pulse_active) {
-    check_pulse_input();
-  }
+  checkPulse();
+//   if (millis() >= lastmillis + 1000) {
+//   Serial.println(millis());
+//   lastmillis = millis();}
+
 }
 
+
+
+// do pulse related opperations
+bool checkPulse() {
+  if (pulse_mode == 1) {
+    check_pulse_input();
+  }
+  else if (pulse_mode == 2) {
+    if (pulse_on) {
+      run_pulse();
+    }
+    else {
+        start_pulse();
+        run_pulse();
+      }
+  }
+  else if (pulse_mode == 0) {
+    if (pulse_on) {
+      stop_pulse();
+    }
+  }
+}
 
 // run flicker
 bool pulse_trigger_value = 0;
 void check_pulse_input() {
   pulse_trigger_value = digitalRead(pulse_trigger_pin);
   if (pulse_on) {
-    if (pulse_trigger_value==0){stop_pulse();}
-    else {run_pulse();}
+    if (pulse_trigger_value == 0) {
+      stop_pulse();
+    }
+    else {
+      run_pulse();
+    }
   }
   else {
-    if (pulse_trigger_value==1){start_pulse();}
+    if (pulse_trigger_value == 1) {
+      start_pulse();
+    }
   }
 }
 
@@ -146,26 +188,26 @@ void start_pulse() {
   digitalWrite(pulse_output_pin, 1);
   pulseout_state = 1;
 }
-void stop_pulse(){
+void stop_pulse() {
   pulse_on = 0;
   digitalWrite(pulse_output_pin, 0);
   pulseout_state = 0;
 }
 void run_pulse() {
   long current_time = micros();
-  if (pulseout_state==1) {
-     if (current_time >= last_up + pulse_width) {
-       digitalWrite(pulse_output_pin, 0);
-       pulseout_state = 0;
-     }
+  if (pulseout_state == 1) {
+    if (current_time >= last_up + pulse_width*1000) {
+      digitalWrite(pulse_output_pin, 0);
+      pulseout_state = 0;
+    }
   }
   else {
-    if (current_time >= last_up + pulse_period) {
-      digitalWrite(pulse_output_pin, 1); 
+    if (current_time >= last_up + pulse_period*1000) {
+      digitalWrite(pulse_output_pin, 1);
       pulseout_state = 1;
-      //last_up = last_up + pulse_period; 
+      //last_up = last_up + pulse_period;
       last_up = current_time;
     }
   }
 }
-  
+
