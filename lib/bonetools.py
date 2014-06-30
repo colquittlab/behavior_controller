afed760b78eb8db47e8f283c213c6248cae8fda6
Jeffrey Knowles <jknowles@albatross.cin.ucsf.edu>
2014-06-30 15:34:34 -0700

    tinychange

--- lib/bonetools.py
+++ lib/bonetools.py
@@ -1,7 +1,7 @@
 import Adafruit_BBIO.GPIO as GPIO
 import Adafruit_BBIO.PWM as PWM
 import time
-
+import execjs as js
 GPIO.cleanup()
 #PWM.start("P9_14", 50, 1)
 GPIO.setup("P8_11", GPIO.IN, GPIO.PUD_UP, 1)
@@ -10,6 +10,13 @@ GPIO.setup("P8_13", GPIO.OUT)
 GPIO.setup("P9_12", GPIO.IN, GPIO.PUD_UP, 1)
 GPIO.add_event_detect("P8_11", GPIO.FALLING)#,bouncetime = 1)
 GPIO.add_event_detect("P9_12", GPIO.FALLING)
+
+bonescript_api = js.compile("""
+	var b = require('bonescrpt')
+	pinmode = b.pinmode
+	""")
+bonscript_api.call(b)
+
 
 # GPIO.add_event_dectect("P9_10", GPIO.FALLINGPIO.add_event_detect("P8_11", GPIO.FALLING, bouncetime = 100000)G,bouncetime = 100)
 import ipdb; ipdb.set_trace()
