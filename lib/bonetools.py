ac4c3847d95adc6b0c6860b31fd6d634dd561fe6
Jeffrey Knowles <jknowles@albatross.cin.ucsf.edu>
2014-06-30 13:04:00 -0700

    tinychange

--- lib/bonetools.py
+++ lib/bonetools.py
@@ -12,7 +12,7 @@ GPIO.add_event_detect("P8_30", GPIO.FALLING)#,bouncetime = 1)
 GPIO.add_event_detect("P9_12", GPIO.FALLING)
 
 # GPIO.add_event_dectect("P9_10", GPIO.FALLINGPIO.add_event_detect("P8_11", GPIO.FALLING, bouncetime = 100000)G,bouncetime = 100)
-# import ipdb; ipdb.set_trace()
+import ipdb; ipdb.set_trace()
 count = 0
 while True:
 	if GPIO.event_detected("P8_30"):
