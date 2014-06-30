72d254e8a7886da5bbef2810a5ba1e6498536069
Jeffrey Knowles <jknowles@albatross.cin.ucsf.edu>
2014-06-30 12:32:05 -0700

    tinychange

--- lib/bonetools.py
+++ lib/bonetools.py
@@ -5,7 +5,7 @@ import time
 #GPIO.cleanup()
 #PWM.start("P9_14", 50, 1)
 GPIO.setup("P8_11", GPIO.IN)
-GPIO.add_event_detect("P8_11", GPIO.RISING, bouncetime = 1e6)#,bouncetime = 1)
+GPIO.add_event_detect("P8_11", GPIO.RISING, bouncetime = 100000)#,bouncetime = 1)
 
 
 # GPIO.add_event_dectect("P9_10", GPIO.FALLING,bouncetime = 100)
