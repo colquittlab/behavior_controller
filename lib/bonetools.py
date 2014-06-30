704ba0243bf45fc2d09d6f5c63f62d0e1d0a2b98
Jeffrey Knowles <jknowles@albatross.cin.ucsf.edu>
2014-06-30 12:44:22 -0700

    tinychange

--- lib/bonetools.py
+++ lib/bonetools.py
@@ -5,11 +5,14 @@ import time
 GPIO.cleanup()
 #PWM.start("P9_14", 50, 1)
 GPIO.setup("P8_11", GPIO.IN)
+GPIO.setup("P8_12", GPIO.IN)
+GPIO.setup("P8_13", GPIO.OUT)
 GPIO.setup("P9_12", GPIO.IN)
 GPIO.add_event_detect("P8_11", GPIO.FALLING)#,bouncetime = 1)
 GPIO.add_event_detect("P9_12", GPIO.FALLING)
 
 # GPIO.add_event_dectect("P9_10", GPIO.FALLINGPIO.add_event_detect("P8_11", GPIO.FALLING, bouncetime = 100000)G,bouncetime = 100)
+import ipdb; ipdb.set_trace()
 count = 0
 while True:
 	if GPIO.event_detected("P8_11"):
