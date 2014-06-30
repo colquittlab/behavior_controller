e726dffb9c40d4efdfa50aaa5d0d387f891b7fb6
Jeffrey Knowles <jknowles@albatross.cin.ucsf.edu>
2014-06-30 12:34:30 -0700

    tinychange

--- lib/bonetools.py
+++ lib/bonetools.py
@@ -2,15 +2,18 @@ import Adafruit_BBIO.GPIO as GPIO
 import Adafruit_BBIO.PWM as PWM
 import time
 
-#GPIO.cleanup()
+GPIO.cleanup()
 #PWM.start("P9_14", 50, 1)
 GPIO.setup("P8_11", GPIO.IN)
-GPIO.add_event_detect("P8_11", GPIO.RISING, bouncetime = 100000)#,bouncetime = 1)
+GPIO.add_event_detect("P8_11", GPIO.FALLING)#,bouncetime = 1)
+GPIO.add_event_detect("P9_12", GPIO.FALLING)
 
-
-# GPIO.add_event_dectect("P9_10", GPIO.FALLING,bouncetime = 100)
+# GPIO.add_event_dectect("P9_10", GPIO.FALLINGPIO.add_event_detect("P8_11", GPIO.FALLING, bouncetime = 100000)G,bouncetime = 100)
 count = 0
 while True:
 	if GPIO.event_detected("P8_11"):
+		count += 1
+		print time.time(), count, 'falling'
+	if GPIO.event_detected("P9_12"):
 		count += 1
 		print time.time(), count, 'falling'
