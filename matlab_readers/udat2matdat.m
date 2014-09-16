3db7530ce57c4e1e2e6ada221d7db5b6137cc1ce
Jeffrey Knowles <jknowles@albatross.cin.ucsf.edu>
2014-09-16 13:19:22 -0700

    great for west coast

--- matlab_readers/udat2matdat.m
+++ matlab_readers/udat2matdat.m
@@ -1,5 +1,6 @@
 function [mat_date, mat_dat_vec] = udat2matdat(unix_date)
-mat_date = (unix_date/86400)+datenum(1970,1,0);
+unix_date = unix_date - 8*60*60; 
+mat_date = (unix_date/86400)+datenum(1970,1,1);
 if nargout > 1
     mat_dat_vec = datevec(mat_date);
 end
