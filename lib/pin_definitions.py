534987dd486d29b06103d220bfc095e6c261a425
Jeffrey Knowles <jknowles@albatross.cin.ucsf.edu>
2015-04-28 11:57:14 -0700

    updated pin defs for cape

--- lib/pin_definitions.py
+++ lib/pin_definitions.py
@@ -3,6 +3,6 @@
 input_definitions = {'P8_11': ['song_trigger'], 
                      'P8_12': ['response_trigger', 'response_a'],
                      'P8_14': ['response_trigger', 'response_b']}
-output_definitions = {'reward_port': 'P8_15',
-                      'light_port': 'P8_17',
-                      'laser_port': 'P8_19'}
+output_definitions = {'reward_port': 'P9_42',
+                      'light_port': 'P9_27',
+                      'laser_port': 'P9_21'}
