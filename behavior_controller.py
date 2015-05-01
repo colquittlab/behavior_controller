4795a4bf573194e9e64e3be54fb7b9345739b33e
Jeffrey Knowles <jknowles@albatross.cin.ucsf.edu>
2015-05-01 15:33:28 -0700

    bonepush

--- behavior_controller.py
+++ behavior_controller.py
@@ -425,7 +425,7 @@ def main_loop(controller, box):
                 controller.store_current_trial()
                 controller.que_next_trial()
 
-            if 'force_feed_up' in pindef.input_definitions.values():
+            if ['force_feed_up'] in pindef.input_definitions.values():
                 key = pindef.input_definitions.keys(pindef.input_definitions.values().index('force_feed_up'))
                 print key
                 if box.force_feed_up is True:
