8e1162c6414ba73277fb02b021190e6c4962a554
Jeffrey Knowles <jknowles@albatross.cin.ucsf.edu>
2015-05-01 12:42:50 -0700

    added relay to feeder_port list

--- lib/pin_definitions.py
+++ lib/pin_definitions.py
@@ -3,6 +3,6 @@
 input_definitions = {'P8_11': ['song_trigger'], 
                      'P8_12': ['response_trigger', 'response_a'],
                      'P8_14': ['response_trigger', 'response_b']}
-output_definitions = {'feeder_port': 'P9_42',
+output_definitions = {'feeder_port': ['P9_42', 'P8_18'],
                       'light_port': 'P9_27',
                       'laser_port': 'P9_21'}
