d8031a95d1ef234d3c7c41279e7338ca338b2b8f
Jeffrey Knowles <jknowles@albatross.cin.ucsf.edu>
2015-05-01 21:35:45 -0700

    stuff

--- behavior_controller.py
+++ behavior_controller.py
@@ -427,16 +427,13 @@ def main_loop(controller, box):
                 controller.store_current_trial()
                 controller.que_next_trial()
 
-            if ['force_feed_up'] in pindef.input_definitions.values():
-                key = pindef.input_definitions.keys()[pindef.input_definitions.values().index(['force_feed_up'])]
+            if 'toggle_force_feed' in events_since_last
                 if box.force_feed_up is False:
-                    if bt.GPIO.input(key)==1:
-                        box.force_feed_up = True
-                        box.feeder_on()
+                    box.force_feed_up = True
+                    box.feeder_on()
                 else:
-                    if bt.GPIO.input(key)==0:
-                        box.force_feed_up = False
-                        box.feeder_off()
+                    box.force_feed_up = False
+                    box.feeder_off()
 
         # exit routine:
         pass
--- lib/pin_definitions.py
+++ lib/pin_definitions.py
@@ -3,7 +3,7 @@
 input_definitions = {'P8_11': ['song_trigger'], 
                      'P8_12': ['response_trigger', 'response_a'],
                      'P8_14': ['response_trigger', 'response_b'],
-                     'P8_15': ['force_feed_up']}
+                     'P8_15': ['toggle_force_feed']}
 output_definitions = {'feeder_port': ['P9_42', 'P8_18'],
                       'light_port': 'P9_27',
                       'laser_port': 'P9_21'
