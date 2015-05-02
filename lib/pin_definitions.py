961dcebf536e93b8b9085aaea41c5eeccdba76fd
Jeffrey Knowles <jknowles@albatross.cin.ucsf.edu>
2015-05-01 21:38:03 -0700

    stuff

--- lib/pin_definitions.py
+++ lib/pin_definitions.py
@@ -6,6 +6,6 @@ input_definitions = {'P8_11': ['song_trigger'],
                      'P8_15': ['toggle_force_feed']}
 output_definitions = {'feeder_port': ['P9_42', 'P8_18'],
                       'light_port': 'P9_27',
-                      'laser_port': 'P9_21'
-                      'error_led': 'P9_24'
-                      'pwm_pin' 'P9_14'}
+                      'laser_port': 'P9_21',
+                      'error_led': 'P9_24',
+                      'pwm_pin': 'P9_14'}
