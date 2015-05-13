b4317de2c9f41ff12b56d4ca72b67e0cb3cdb065
Jeffrey Knowles <jknowles@albatross.cin.ucsf.edu>
2015-05-12 19:03:49 -0700

    stuff

--- bonebootstrap.sh
+++ bonebootstrap.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
 # set password for root
-passwd root
+while true; do
+    read -p "Do you wish to set password for root? [Y/N] (you should!!)\n" yn
+    case $yn in
+        [Yy]* ) echo "set password for root"; passwd root;;
+        [Nn]* ) exit;;
+    esac
+done
 # create users as desired
 while true; do
-    read -p "Do you wish to make a new (sudo) user? [Y/N]" yn
+    read -p "Do you wish to make a new (sudo) user? [Y/N]\n" yn
     case $yn in
-        [Yy]* ) read -p "Enter User name" un; adduser $un --ingroup sudo; passwd $un; break;;
+        [Yy]* ) read -p "Enter User name\n" un; adduser $un --ingroup sudo; passwd $un;;
         [Nn]* ) exit;;
     esac
 done
