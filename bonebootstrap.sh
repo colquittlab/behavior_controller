42e30192a3fbb4d3a40d6de142540fd2bf0f51c2
Jeffrey Knowles <jknowles@albatross.cin.ucsf.edu>
2015-05-12 19:11:12 -0700

    stuff

--- bonebootstrap.sh
+++ bonebootstrap.sh
@@ -2,7 +2,7 @@
 
 # set password for root
 while true; do
-    read -p "Do you wish to set password for root? [Y/N] (you should!!)\n" yn
+    read -p "Do you wish to set password for root? [Y/N] (you should!!)  " yn
     case $yn in
         [Yy]* ) echo "set password for root"; passwd root; break;;
         [Nn]* ) exit;;
@@ -10,21 +10,18 @@ while true; do
 done
 # create users as desired
 while true; do
-    read -p "Do you wish to make a new (sudo) user? [Y/N]\n" yn
+    read -p "Do you wish to make a new (sudo) user? [Y/N]  " yn
     case $yn in
-        [Yy]* ) read -p "Enter User name\n" un; adduser $un --ingroup sudo; passwd $un;;
+        [Yy]* ) read -p "Enter User name\n" un; adduser $un --ingroup sudo;;
         [Nn]* ) exit;;
     esac
 done
-
-
-
+echo "hello"
 
 
 APTPACKAGES="python-scipy python-alsaaudio"
 PIPPACKAGES="pyexecjs pyserial"
 apt-get update
-
 for PACK in $APTPACKAGES
 do
     apt-get --assume-yes install $PACK
