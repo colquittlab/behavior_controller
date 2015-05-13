1e5b5b020ff8606fd24a320f873095075dec5ac9
Jeffrey Knowles <jknowles@albatross.cin.ucsf.edu>
2015-05-12 19:29:27 -0700

    stuff

--- bonebootstrap.sh
+++ bonebootstrap.sh
@@ -5,7 +5,7 @@ while true; do
     read -p "Do you wish to set password for root? [Y/N] (you should!!)  " yn
     case $yn in
         [Yy]* ) echo "set password for root"; passwd root; break;;
-        [Nn]* ) exit;;
+        [Nn]* ) break;;
     esac
 done
 # create users as desired
@@ -13,11 +13,26 @@ while true; do
     read -p "Do you wish to make a new (sudo) user? [Y/N]  " yn
     case $yn in
         [Yy]* ) read -p "Enter User name\n" un; adduser $un --ingroup sudo;;
-        [Nn]* ) exit;;
+        [Nn]* ) break;;
     esac
 done
-echo "hello"
 
+# setup eth0 interface
+while true; do
+    read -p "Do you wish to setup eth0 as static ip? [Y/N]  " yn
+    case $yn in
+        [Yy]* ) read -p "Enter address" staticaddress;
+                netmask=255.255.254.0;
+                gateway=169.230.190.1;
+                dns-nameservers=169.230.190.10;
+                dns-search=cin.ucsf.edu;
+                awk -f lib/changeInterface.awk /etc/network/interfaces device=eth0 adress=$staticaddress netmask=$netmask gateway=$gateway
+                break;;
+        [Nn]* ) break;;
+    esac
+done
+
+# setup usb0 interface
 
 APTPACKAGES="python-scipy python-alsaaudio"
 PIPPACKAGES="pyexecjs pyserial"
