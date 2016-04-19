f3d50a313bda87b067b95ac7c30234b213249400
Jeffrey Knowles <jknowles@albatross.cin.ucsf.edu>
2016-04-18 17:23:12 -0700

    fixed bug due to no choices

--- behavior_controller.py
+++ behavior_controller.py
@@ -259,9 +259,10 @@ class BehaviorController(object):
                 stats['by_stimset'][stimset_idx]['p_correct'] = 0
             else:
                 stats['by_stimset'][stimset_idx]['p_correct'] = float(stats['by_stimset'][stimset_idx]['n_correct']) / (stats['by_stimset'][stimset_idx]['n_correct'] + stats['by_stimset'][stimset_idx]['n_incorrect'])
-        
-            stats['by_stimset'][stimset_idx]['p_occurance'] = float(stats['by_stimset'][stimset_idx]['n_occurances']) / stats['nchoices']
-        
+            if stats['nchoices'] > 0: 
+                stats['by_stimset'][stimset_idx]['p_occurance'] = float(stats['by_stimset'][stimset_idx]['n_occurances']) / stats['nchoices']
+            else:
+                stats['by_stimset'][stimset_idx]['p_occurance'] = 0.5
         return stats    
         
 
