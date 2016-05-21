41bf367ba3afe22342aed5dbd3ee1cabae287ec0
Jeffrey Knowles <jknowles@albatross.cin.ucsf.edu>
2016-05-21 12:10:44 -0700

    stuff

--- schedule_template.py
+++ schedule_template.py
@@ -44,10 +44,13 @@ def start_box(config_file, randomize_stimsets=False):
 
 
 def stop_box():	
-	print "stopping box"
-	global active_controller, active_box
-	active_controller.box_state = 'stop'
-	active_box.light_off()
+	try:
+		print "stopping box"
+		global active_controller, active_box
+		active_controller.box_state = 'stop'
+		active_box.light_off()
+	except:
+		pass
 
 
 def start_task_randomly():
@@ -108,7 +111,7 @@ schedule.every().day.at('10:01').do(start_box,'bsp_tnull')
 schedule.every().day.at('10:02').do(eval,'active_box.light_off()')
 
 schedule.every().day.at('10:59').do(stop_box)
-schedule.every().day.at('11:52').do(start_box,0,randomize_stimsets=True)
+schedule.every().day.at('11:00').do(start_box,0,randomize_stimsets=True)
 schedule.every().day.at('12:00').do(stop_box)
 schedule.every().day.at('12:01').do(start_box,'bsp_tnull')
 schedule.every().day.at('12:02').do(eval,'active_box.light_off()')
