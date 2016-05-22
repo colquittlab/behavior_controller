20ec1e5eb88aae911c5591bc5248e8925336562c
root <root@beaglebone.(none)>
2016-05-21 22:10:35 -0700

    stuffstuff

--- schedule_template.py
+++ schedule_template.py
@@ -107,32 +107,32 @@ def printme(session_num=0):
 schedule.every().day.at('00:01').do(generate_sessions_for_day)
 schedule.every().day.at('09:00').do(start_box,0,randomize_stimsets=True)
 schedule.every().day.at('10:00').do(stop_box)
-schedule.every().day.at('10:01').do(start_box,'bsp_tnull')
+schedule.every().day.at('10:01').do(start_box,1)
 schedule.every().day.at('10:02').do(eval,'active_box.light_off()')
 
 schedule.every().day.at('10:59').do(stop_box)
 schedule.every().day.at('11:00').do(start_box,0,randomize_stimsets=True)
 schedule.every().day.at('12:00').do(stop_box)
-schedule.every().day.at('12:01').do(start_box,'bsp_tnull')
+schedule.every().day.at('12:01').do(start_box,1)
 schedule.every().day.at('12:02').do(eval,'active_box.light_off()')
 
 schedule.every().day.at('12:59').do(stop_box)
 schedule.every().day.at('13:00').do(start_box,0,randomize_stimsets=True)
 schedule.every().day.at('14:00').do(stop_box)
-schedule.every().day.at('14:01').do(start_box,'bsp_tnull')
+schedule.every().day.at('14:01').do(start_box,1)
 schedule.every().day.at('14:02').do(eval,'active_box.light_off()')
 
 
 schedule.every().day.at('14:59').do(stop_box)
 schedule.every().day.at('15:00').do(start_box,0,randomize_stimsets=True)
 schedule.every().day.at('16:00').do(stop_box)
-schedule.every().day.at('16:01').do(start_box,'bsp_tnull')
+schedule.every().day.at('16:01').do(start_box,1)
 schedule.every().day.at('16:02').do(eval,'active_box.light_off()')
 
 schedule.every().day.at('16:59').do(stop_box)
 schedule.every().day.at('17:00').do(start_box,0,randomize_stimsets=True)
 schedule.every().day.at('18:00').do(stop_box)
-schedule.every().day.at('18:01').do(start_box,'bsp_tnull')
+schedule.every().day.at('18:01').do(start_box,1)
 schedule.every().day.at('18:02').do(eval,'active_box.light_off()')
 
 
